34ca2425b133c23e17748399fc5496d3183bfcd9
Mark Chambers <cmark6466@gmail.com>
2024-11-05 18:28:16 -0500

    Posting

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, url_for
-# from flask_bootstrap import Bootstrap5
+from flask import Flask, render_template
 from jobdict import jobs
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SUPER_SECRET_KEY = os.getenv("SUPER_SECRET_KEY")
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'lkasjdflkjasdflkjasdflkj'
-# Bootstrap5(app)
+app.config['SECRET_KEY'] = SUPER_SECRET_KEY
+
 
 
 @app.route('/')
@@ -13,4 +19,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=False)
